83de2373a1545a1ab7644b36c63019b73816e6fd
vidhisharma3193 <vidhisharma3193@gmail.com>
2021-03-15 18:48:26 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+import { useState, useEffect } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import NavBar from './NavBar';
 import PaintingsList from './PaintingsList';
-import paintings from './painting_data'
+// import paintings from './painting_data'
 
 
 
 function App() {
+
+  const [color, setColor] = useState("red") // State
+  const [paintings, setPaintings] = useState([]) // State
+
+  useEffect(() => {
+    fetch("http://localhost:3000/paintings")
+    .then(res => res.json())
+    .then(paintings => setPaintings(paintings))
+  },[]) // ComponentDidMount
+
+  const changeColor = () => {
+    setColor("blue")
+  }
+
   return (
     <div>
       <NavBar
-        color="red"
+        color={color}
         title="Paintr"
         icon="paint brush"
         description="an app we made"
+        changeColor={changeColor}
       />
+      {/* updating the state*/}
+      {/* <button onClick={() => setColor("blue")}>Change color</button>  */}
+
       <PaintingsList paintings={paintings} />
     </div>
   );
--- src/Painting.js
+++ src/Painting.js
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 const Painting = props => {
+
+  const [votes, addVote] = useState(props.painting.votes)
+
   return (
     <div>
       <img src={props.painting.image} />
@@ -9,6 +14,15 @@ const Painting = props => {
       <p>
         Dimensions: {props.painting.dimensions.width} in. x {props.painting.dimensions.height} in.
       </p>
+
+      <div class="ui labeled button" tabindex="0">
+          <div class="ui red button" onClick={() => addVote(votes+1)}>
+            <i class="heart icon"></i> Add Vote
+          </div>
+          <a class="ui basic red left pointing label">
+            {votes}
+          </a>
+      </div>
     </div>
       
   );
